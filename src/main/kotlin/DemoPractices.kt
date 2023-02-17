41baab57159f893fda10242b4e4d10c126ad3c98
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2023-02-17 07:36:50 +0100

    add demo kotlin practices

---
+++ src/main/kotlin/DemoPractices.kt
@@ -0,0 +1,19 @@
+package main.kotlin
+
+class DemoPractices {
+
+    class Car(val color: String) {
+        private val car1 = Car("Black")
+        private val car2 = Car("Black")
+        private val car3 = car1
+
+        fun main() {
+            print(car1 == car2) // false ( the equals() method not invoked as it's not a data class)
+            print(car1 == car3) // true
+            print(car1 === car2) // true
+            println(car1 === car3) // true (reference equality)
+        }
+    }
+
+
+}
