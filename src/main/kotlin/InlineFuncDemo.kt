0a637b896a4b055c02375454f94b1f584a795633
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2023-02-20 06:30:44 +0100

    create kotlin inline func demo

---
+++ src/main/kotlin/InlineFuncDemo.kt
@@ -0,0 +1,16 @@
+package main.kotlin
+
+class InlineFuncDemo {
+
+    fun guide() {
+        println("Guide Start!")
+        teach()
+        println("Guide ended")
+    }
+
+    private fun teach() {
+
+    }
+
+
+}
