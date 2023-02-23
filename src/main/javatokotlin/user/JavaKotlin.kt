1d40c8c07bed8d7aa3b9962c5da42178418e5a8c
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2023-02-23 07:11:05 +0100

    update loops

--- src/main/javatokotlin/user/JavaKotlin.kt
+++ src/main/javatokotlin/user/JavaKotlin.kt
@@ -146,18 +146,23 @@ class JavaKotlin {
             */
 
         for (i in 1..10) {
+            print(i)
         }
 
         for (i in 1 until 10) {
+            print(i)
         }
 
         for (i in 10 downTo 0) {
+            print(i)
         }
 
         for (i in 1..10 step 2) {
+            print(i)
         }
 
         for (i in 10 downTo 0 step 2) {
+            print(i)
         }
 
 /*      for (String item : collection) { }
