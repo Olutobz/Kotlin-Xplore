3709110163f3d06e0ad38c3cc7a94ce7f4941d9f
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2024-04-24 16:59:04 +0100

    create interface and util files

---
+++ src/main/javatokotlin/SampleInterface.kt
@@ -0,0 +1,14 @@
+package main.javatokotlin
+
+/**
+ * Created by Onikoyi Damola Olutoba
+ * DATE: 24 April 2024
+ * EMAIL: damexxey94@gmail.com
+ */
+interface SomeInterface<T> {
+    fun doSomething(data: T)
+}
+
+interface SomeInterfaceTwo<T : Collection<*>> {
+    fun doSomething(data: T)
+}
---
+++ src/main/javatokotlin/Utils.kt
@@ -0,0 +1,15 @@
+package main.javatokotlin
+
+class Utils private constructor() {
+    companion object {
+        fun getScore(value: Int): Int {
+            return 2 * value
+        }
+    }
+}
+
+object Util {
+    fun getScore(value: Int): Int {
+        return 2 * value
+    }
+}
