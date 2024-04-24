92a5d66681c7716153b8c90043a977bbd356dc9c
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2024-04-24 23:01:17 +0100

    impl method to retrieve formattedName

--- src/main/javatokotlin/Data.kt
+++ src/main/javatokotlin/Data.kt
@@ -3,3 +3,35 @@ package main.javatokotlin
 data class User(var firstName: String?, var lastName: String?)
 
 data class Developer(var name: String, var age: Int)
+
+val User.userFormattedName: String
+    get() {
+        return if (lastName != null) {
+            if (firstName != null) {
+                "$firstName $lastName"
+            } else {
+                lastName ?: "Unknown"
+            }
+        } else {
+            firstName ?: "Unknown"
+        }
+    }
+
+fun User.getFormattedName(): String {
+    return if (lastName != null) {
+        if (firstName != null) {
+            "$firstName $lastName"
+        } else {
+            lastName ?: "Unknown"
+        }
+    } else {
+        firstName ?: "unknown"
+    }
+}
+
+fun getDeveloper(): Developer {
+    return Developer(name = "toba", age = 12).apply {
+        name = "Olutoba"
+        age = 27
+    }
+}
