e48bed104ec7515d14d5fd50287a9c5642c28a32
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2024-04-25 18:29:01 +0100

    change to data object for sealed classes

--- src/main/practice/xplore/Seal.kt
+++ src/main/practice/xplore/Seal.kt
@@ -1,12 +1,12 @@
 package main.practice.xplore
 
 sealed class Seal
-object SeaLion : Seal()
-object Walrus : Seal()
+data object SeaLion : Seal()
+data object Walrus : Seal()
+
 
 fun matchSeal(seal: Seal): String {
     return when (seal) {
-        // type checks
         is SeaLion -> "sea lion"
         is Walrus -> "walrus"
     }
