15f9ab2ff605f32d1e6e2ef30a8314006df8ef6a
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2025-07-31 21:58:24 +0100

    add 'when' sample

--- src/main/practice/test/Sample.kt
+++ src/main/practice/test/Sample.kt
@@ -229,6 +229,17 @@ private fun fishFood(day: String): String {
     }
 }
 
+fun testWhenCondition() {
+    val x: Any = 4
+
+    when (x) {
+        2, 3, 5, 7 -> println("x is a prime number between 1 and 10.")
+        in 1..10 -> println("x is a number between 1 and 10, but not a prime number.")
+        is Int -> println("x is an integer number, but not between 1 and 10.")
+        else -> println("x isn't a prime number between 1 and 10.")
+    }
+}
+
 private fun giveMeATool(): Pair<String, String> {
     return ("fishnet" to "catching fish")
 }
