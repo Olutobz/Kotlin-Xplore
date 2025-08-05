d750f1fbfa71c1a4555f27ac5b5483d78e6c4358
Damola Olutoba Onikoyi <damexxey94@gmail.com>
2025-08-06 00:06:56 +0100

    encapsulate deviceTurnOnCount

---
+++ src/main/practice/xplore/SmartDevice.kt
@@ -0,0 +1,80 @@
+package main.practice.xplore
+
+/**
+ * Created by Damola Olutoba Onikoyi
+ * DATE: 05, August 2025
+ * EMAIL: damexxey94@gmail.com
+ */
+
+
+open class SmartDevice(val name: String, val category: String) {
+    var deviceStatus = "online"
+
+    constructor(name: String, category: String, statusCode: Int) : this(name, category) {
+        deviceStatus = when (statusCode) {
+            0 -> "offline"
+            1 -> "online"
+            else -> "unknown"
+        }
+    }
+
+    open fun turnOn() {
+        deviceStatus = "online"
+    }
+
+    open fun turnOff() {
+        deviceStatus = "offline"
+    }
+
+}
+
+class SmartTvDevice(deviceName: String, deviceCategory: String) :
+    SmartDevice(name = deviceName, category = deviceCategory) {
+
+    private var speakerVolume = 4
+        set(value) {
+            if (value in 0..100) {
+                field = value
+            }
+        }
+
+    private var channelNumber = 19
+        set(value) {
+            if (value in 0..200) {
+                field = value
+            }
+        }
+
+    var deviceTurnOnCount = 0
+        private set
+
+    fun increaseVolume() {
+        speakerVolume++;
+        println("Speaker volume increased to $speakerVolume")
+    }
+
+    fun nextChannel() {
+        channelNumber++;
+        println("Change number increased to $channelNumber")
+    }
+
+    override fun turnOn() {
+        super.turnOn()
+        deviceTurnOnCount++
+        println("$name is turned on.")
+    }
+
+    override fun turnOff() {
+        super.turnOff()
+        deviceTurnOnCount--
+        println("$name is turned off.")
+    }
+
+}
+
+class SmartHome(val smartTvDevice: SmartTvDevice) {
+
+    fun increaseTvVolume() {
+        smartTvDevice.increaseVolume()
+    }
+}
